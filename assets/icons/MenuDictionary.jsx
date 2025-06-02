b9cae12fc34cad59fb4522a5523444b598359612
LebedevIgor <igorlebedev018@gmail.com>
2025-06-02 04:17:04 +0300

    fix

--- assets/icons/MenuDictionary.jsx
+++ assets/icons/MenuDictionary.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Svg, { G, Path, Defs, ClipPath } from 'react-native-svg';
+
 const MenuDictionary = (props) => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
@@ -19,9 +20,9 @@ const MenuDictionary = (props) => (
       />
     </G>
     <Defs>
-      <clipPath id="a">
-        <path fill="#fff" d="M0 0h48v48H0z" />
-      </clipPath>
+      <ClipPath id="a">
+        <Path fill="#fff" d="M0 0h48v48H0z" />
+      </ClipPath>
     </Defs>
   </Svg>
 );
--- src/components/Tabs/styled.js
+++ src/components/Tabs/styled.js
@@ -15,15 +15,25 @@ export const tabRouterStyles = StyleSheet.create({
     shadowOpacity: 0,
     shadowRadius: 0,
     borderRadius: 0,
-    height: 60,
+    height: 80,
     justifyContent: 'center',
     marginTop: -1,
     paddingTop: 1,
+    overflow: 'visible',
+    paddingHorizontal: 0,
   },
 
   'tab-label': {
     color: Colors.green,
     fontSize: 12,
     fontWeight: '500',
+  },
+
+  'tab-item': {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 'auto',
+    height: 'auto',
+    overflow: 'visible',
   },
 });
